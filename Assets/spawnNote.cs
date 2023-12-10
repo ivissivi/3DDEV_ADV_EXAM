68da22bea62b841af4a6c7e0e7ff4cac951bb575
ivissivi <ivis31121999@gmail.com>
2023-12-10 23:52:56 +0200

    Minor tweaks for object size

--- Assets/spawnNote.cs
+++ Assets/spawnNote.cs
@@ -4,64 +4,57 @@ using UnityEngine;
 
 public class SpawnNote : MonoBehaviour
 {
-    public GameObject notePrefab; // Assign the prefab for the object to be spawned
-    public float speed = 2.0f; // Movement speed of the spawned object
-    public float destroyCoordinate = -2.3f; // Z-coordinate to delete the object
-    public float scaleMultiplier = 1.0f; // Inspector value to adjust the scale of the prefab
-    public KeyCode spawnKey; // Keycode to spawn the object, default is Z
-    public Color parentColor; // Color for the parent object
+    public GameObject notePrefab;
+    public float speed = 2.0f;
+    public float destroyCoordinate = -1.3f;
+    public float scaleMultiplier = 1.0f;
+    public Color parentColor;
 
-    private GameObject currentNote; // Reference to the spawned object
-    private Color originalColor; // Store the original color of the parent object
+    private Color originalColor;
+    private Renderer parentRenderer;
+    private int activeObjects = 0;
 
     void Start()
     {
-        // Store the original color of the parent object
-        Renderer parentRenderer = GetComponent<Renderer>();
+        parentRenderer = GetComponent<Renderer>();
         if (parentRenderer != null)
         {
             originalColor = parentRenderer.material.color;
         }
+
+        StartCoroutine(SpawnObjectWithDelay());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnObjectWithDelay()
     {
-        // Check for key press to spawn the object
-        if (Input.GetKeyDown(spawnKey))
+        while (true)
         {
             SpawnObject();
+            yield return new WaitForSeconds(3.0f);
         }
     }
 
     void SpawnObject()
     {
-        currentNote = Instantiate(notePrefab, transform.position, Quaternion.identity);
-
-        // Adjust the scale of the spawned object by the Inspector value
+        GameObject currentNote = Instantiate(notePrefab, transform.position, Quaternion.identity);
         currentNote.transform.localScale = notePrefab.transform.localScale * scaleMultiplier;
-
-        // Set the spawned object's position to match the parent's position
         currentNote.transform.position = transform.position;
 
+        activeObjects++; // Increment count of active objects
         StartCoroutine(MoveObject(currentNote));
     }
 
     IEnumerator MoveObject(GameObject obj)
     {
-        Renderer parentRenderer = GetComponent<Renderer>();
-
         while (obj.transform.position.z > destroyCoordinate)
         {
             obj.transform.Translate(Vector3.back * speed * Time.deltaTime);
 
-            // Calculate distance to the destroy coordinate
             float distance = obj.transform.position.z - destroyCoordinate;
 
-            // Calculate reversed color change based on distance
             if (parentRenderer != null)
             {
-                float colorChangeFactor = 1.0f - Mathf.Clamp01(distance / 10f); // Reverse the color change factor
+                float colorChangeFactor = 1.0f - Mathf.Clamp01(distance / 10f);
                 parentRenderer.material.color = Color.Lerp(originalColor, parentColor, colorChangeFactor);
             }
 
@@ -69,11 +62,18 @@ public class SpawnNote : MonoBehaviour
         }
 
         Destroy(obj);
+        activeObjects--; // Decrement count of active objects
+        if (activeObjects == 0)
+        {
+            ResetParentColor(); // Reset parent's color if no active objects remain
+        }
+    }
 
-        // Reset the parent's color to the original color when the object is destroyed
+    void ResetParentColor()
+    {
         if (parentRenderer != null)
         {
-            parentRenderer.material.color = originalColor; // Reset to original color
+            parentRenderer.material.color = originalColor;
         }
     }
 }
